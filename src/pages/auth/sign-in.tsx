491ce85efda5c16dbf7835bad796b49e1b1ecea8
Natanael <natanael.silva@instivo.com.br>
2023-12-28 16:21:13 -0300

    feat: added sign-up page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -14,16 +15,26 @@ const signInForm = z.object({
 type SignInForm = z.infer<typeof signInForm>
 
 export function SignIn() {
+  const navigate = useNavigate()
   const { register, handleSubmit } = useForm<SignInForm>()
 
   function handleSignIn(data: SignInForm) {
-    toast.success('Enviamos um link de autentição para seu email')
+    console.log(data)
+    toast.success('Enviamos um link de autentição para seu email', {
+      action: {
+        label: 'Login',
+        onClick: () => navigate('/sign-in'),
+      },
+    })
   }
   return (
     <>
       <Helmet title="Login" />
       <div className="p-8">
-        <div className="flex w-[320px] flex-col justify-center gap-6">
+        <Button variant="outline" asChild className="absolute right-8 top-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
+        <div className="flex w-[340px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-bold tracking-tight">
               Acessar Painel
@@ -36,11 +47,11 @@ export function SignIn() {
           <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" type="email" />
+              <Input id="email" type="email" {...register('email')} />
             </div>
 
             <Button className="w-full" type="submit">
-              Accesar painel
+              Acessar painel
             </Button>
           </form>
         </div>
