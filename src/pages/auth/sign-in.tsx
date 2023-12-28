e58f4fc47139f3f7a91d23dd284b9aab37cd77c5
Natanael <natanael.silva@instivo.com.br>
2023-12-28 15:28:55 -0300

    feat: added toast app

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,24 @@
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
+const signInForm = z.object({
+  email: z.string().email(),
+})
+
+type SignInForm = z.infer<typeof signInForm>
+
 export function SignIn() {
+  const { register, handleSubmit } = useForm<SignInForm>()
+
+  function handleSignIn(data: SignInForm) {
+    toast.success('Enviamos um link de autentição para seu email')
+  }
   return (
     <>
       <Helmet title="Login" />
@@ -19,7 +33,7 @@ export function SignIn() {
             </p>
           </div>
 
-          <form className="space-y-4">
+          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
               <Input id="email" type="email" />
