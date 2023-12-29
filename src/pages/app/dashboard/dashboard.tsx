ab7f24d54b75e6089ef2a79fdaf84f05ab6fae9b
Natanael <natanael.silva@instivo.com.br>
2023-12-28 23:54:57 -0300

    feat: added charts to dashboard

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -4,6 +4,8 @@ import { DayOrderAmountCard } from './day-order-amout-card'
 import { MonthCanceledOrderAmountCard } from './month-cenceled-order-amout-card'
 import { MonthOrderAmountCard } from './month-order-amout-card'
 import { MonthRevenueCard } from './month-revenue-card'
+import { PopularProductsChart } from './popular-products-charts'
+import { RevenueChart } from './revenue-charts'
 
 export function Dashboard() {
   return (
@@ -16,6 +18,11 @@ export function Dashboard() {
         <MonthOrderAmountCard />
         <DayOrderAmountCard />
         <MonthCanceledOrderAmountCard />
+      </div>
+
+      <div className="grid grid-cols-9 gap-4">
+        <RevenueChart />
+        <PopularProductsChart />
       </div>
     </>
   )
